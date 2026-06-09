925af4814faaf7ad699ba22d6872a986775cd822
Yatharth <sachdevayatharth12@gmail.com>
2026-06-10 00:34:54 +0530

    Added Swagger documentation

--- vendor-management-platform/src/main/java/com/yatharth/vmp/config/SecurityConfig.java
+++ vendor-management-platform/src/main/java/com/yatharth/vmp/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**","/swagger-ui/**","/swagger-ui.html","/v3/api-docs","/v3/api-docs/swagger-config").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
